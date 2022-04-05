d77356c659f4f1bf6857f0b63e08a1a6c46e1620
HartMason <hartmason85@gmail.com>
2022-04-05 16:38:55 -0500

    initialize.sql data put in

--- initialize.sql
+++ initialize.sql
@@ -1,21 +1,12 @@
-CREATE TABLE user info (
+CREATE TABLE user_info (
     user_id INT NOT NULL AUTO_INCREMENT,
     user_email VARCHAR(50),
     user_password VARCHAR(50),
-    PRIMARY KEY (id)
+    PRIMARY KEY (user_id)
 );
 
-CREATE TABLE order details (
-    order_num INT NOT NULL AUTO_INCREMENT,
-    shirt_id VARCHAR (50),
-    order_date VARCHAR (50),
-    total_amount VARCHAR (50),
-    user_email VARCHAR (50),
-    PRIMARY KEY (order_num),
-    FOREIGN KEY (shirt_id)
-)
 
-CREATE TABLE order items (
+CREATE TABLE order_items (
     shirt_id VARCHAR (50),
     shirt_name VARCHAR (50),
     shirt_quantity VARCHAR (50),
@@ -23,3 +14,39 @@ CREATE TABLE order items (
     PRIMARY KEY (shirt_id)
 )
 
+CREATE TABLE order_details (
+    order_num INT NOT NULL AUTO_INCREMENT,
+    shirt_id VARCHAR (50) NOT NULL,
+    order_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    total_amount VARCHAR (50),
+    user_id INT NOT NULL,
+    PRIMARY KEY (order_num),
+    FOREIGN KEY (shirt_id)
+    FOREIGN KEY (user_id)
+    REFRENCES order_items(shirt_id)
+)
+
+
+
+INSERT INTO user_info (user_email, user_password) 
+    VALUES ("example@gmail.com", "password54321"),
+    ("jason@gmail.com", "password654321"),
+    ("example@gmail.com", "password54321"),
+    ("example@gmail.com", "password54321")
+
+
+
+INSERT INTO order_items (shirt_id, shirt_name, shirt_price, shirt_quantity)
+    VALUES ("2", "moonShirt", "20.99", "7"),
+    ("3", "campingShirt", "20.99", "4"),
+    ("2", "animalShirt", "20.99", "10"),
+    ("2", "dragonShirt", "20.99", "2")
+
+
+
+INSERT INTO order_details (shirt_id, total_amount, user_id)
+    VALUES ("45", "20.99", "1"),
+    ("45", "20.99", "2"),
+    ("33", "20.99", "3"),
+    ("23", "20.99", "4"),
+    ("55", "20.99", "5")
