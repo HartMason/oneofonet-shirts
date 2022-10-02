8f17b60d5db3fed3a527f774090ef18bf919aa59
HartMason <hartmason85@gmail.com>
2022-10-02 16:23:50 -0500

    added time out

--- sql/connection.js
+++ sql/connection.js
@@ -1,27 +1,28 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
 
 class Connection {
-    constructor() {
-      if (!this.pool) {
-        console.log("creating connection...");
-        this.pool = mysql.createPool({
-          connectionLimit: 100,
-          host: DB_HOST,
-          user: DB_USER,
-          password: DB_PASSWORD,
-          database: DB_DATABASE,
-        });
-  
-        return this.pool;
-      }
-  
+  constructor() {
+    if (!this.pool) {
+      console.log("creating connection...");
+      this.pool = mysql.createPool({
+        connectionLimit: 100,
+        timeout: 100000,
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_DATABASE,
+      });
+
       return this.pool;
     }
+
+    return this.pool;
   }
+}
 
 const instance = new Connection();
 
-module.exports = instance; 
+module.exports = instance;
