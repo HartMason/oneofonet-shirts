ef5474c09f589f52eaa8ee64124b6190d698b46d
HartMason <hartmason85@gmail.com>
2022-10-02 16:36:57 -0500

    changed connection.js

--- sql/connection.js
+++ sql/connection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
+// const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
 
 class Connection {
   constructor() {
@@ -10,10 +10,10 @@ class Connection {
       this.pool = mysql.createPool({
         connectionLimit: 100,
         timeout: 100000,
-        host: DB_HOST,
-        user: DB_USER,
-        password: DB_PASSWORD,
-        database: DB_DATABASE,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
       });
 
       return this.pool;
