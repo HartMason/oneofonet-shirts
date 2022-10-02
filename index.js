88a307fb11f9941ce7fbbcf8605b2830d6f1001f
HartMason <hartmason85@gmail.com>
2022-10-02 15:51:53 -0500

    added to index.js

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const orderDetailsRouter = require("./routes/orderDetails");
 const port = process.env.PORT || 4001;
 const cors = require("cors");
 const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use(express.static("public"));
 app.use(
   express.json({
     type: ["application/json", "text/plain"],
@@ -19,14 +21,12 @@ app.use("/shirts", shirtsRouter);
 app.use("/orderDetails", orderDetailsRouter);
 // app.use("/auth", authRouter)
 
-app.get("/default", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Welcome to our server!");
 });
+
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Web server is listening on port ${port}!`);
 });
-
-// app.use(bodyParser.json());
-
-// app.use(bodyParser.urlencoded({extended: false}))
